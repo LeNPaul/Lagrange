f809d398d80403d16cb70794a76eb741882e600f
anseljohn <jjohnanselmo@gmail.com>
2021-12-11 17:56:31 -0600

    Integrade chakra

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
-import {Box} from "@chakra-uo/react"
+import {Box, Container} from "@chakra-uo/react"
 
 const Main = ({children, router}) => {
     return (
         <Box as="main" pb={8}
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <title>John Anselmo - Homepage</title>
+            </Head>
+
+            <Container maxWidth="container.md" pt={14}>
+                {children}
+            </Container>
+        </Box>
     )
 }
+
+export default Main
